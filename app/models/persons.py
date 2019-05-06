04534424f852b931e54bf4eda325ec29d67a9e22
PTeran <pablo_teran_rios@hotmail.com>
2019-05-06 09:09:36 -0500

    app 2

---
+++ app/models/persons.py
@@ -0,0 +1,72 @@
+from pymongo import MongoClient
+from bson import ObjectId
+from flask import request, url_for, jsonify
+import config
+
+class Persons(object):
+
+    def __init__(self):
+        client = MongoClient(config.MONGO_URI)
+        db = client.db
+        self.collection = db.person
+
+    def insert_user(self,id,email,name,company,age,phone):
+
+        userJson = { "_id": id, "name": name, "company": company, "email": email, "age":age, "phone":phone, "knows":[]}
+        self.collection.insert_one(userJson)
+
+
+    def find(self):
+        """
+        Obtener todos
+        """
+        cursor = self.collection.find()
+
+        usuarios = []
+
+        for usuario in cursor:
+            # Se adicionó para poder manejar ObjectID
+            usuario['_id'] = str(usuario['_id'])
+            usuarios.append(usuario)
+
+        return usuarios
+
+    def findOne(self, email):
+        """
+        Desplegar toda tu informacion
+        """
+        usuario = self.collection.find({'email': email})
+
+        if usuario is not None:
+            usuario['_id'] = str(usuario['_id'])
+
+        return usuario
+
+    def findWhere(self, name):
+        """
+        Buscar personas que tengan la letra K en su nombre
+        """
+        where = "/{}/".format(name)
+
+        usuario = self.collection.find({'name': where})
+        usuarios = []
+
+        if usuario is not None:
+            usuario['_id'] = str(usuario['_id'])
+            usuarios.append(usuario)
+
+        return usuarios
+
+    def findFriends(self, name):
+        """
+        Despliega a tus conocidos
+        """
+        friends = [{$match:{ 'name': {} }}, {$project:{'_id':0, 'knows':1}}]);
+        #QUERY NO TERMINADO
+
+        usuario = self.collection.aggregate(friends)
+
+        if usuario is not None:
+            usuario['_id'] = str(usuario['_id'])
+
+        return usuario
