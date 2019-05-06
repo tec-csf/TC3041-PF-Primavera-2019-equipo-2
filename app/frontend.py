79efd2b30dcb659c347f869420556a4f657813d9
ManuGuV <manologuadarramav@hotmail.com>
2019-05-06 11:00:41 -0500

    Update frontend.py
    Added functionality and other operations

--- app/frontend.py
+++ app/frontend.py
@@ -1,6 +1,8 @@
 from flask import Flask,jsonify,request,make_response,redirect,url_for,render_template, session
 from flask_bootstrap import Bootstrap
 from models import sessions, persons
+from pymongo import MongoClient
+from flask_api import FlaskAPI, status, exceptions
 import jinja2
 import os
 env = jinja2.Environment()
@@ -8,35 +10,15 @@ env.globals.update(zip=zip)
 SECRET_KEY = os.urandom(32)
 id = 9
 
-
-app = Flask(__name__)
+app = FlaskAPI(__name__)
 app.config['SECRET_KEY'] = SECRET_KEY
 Bootstrap(app)
-app.jinj
-        id = id + 1
-        #a.insert_user(id,email,name,company,age,phone)
-
-        return redirect(url_for('login'))
-
-    return render_template('templates/register.html')
-a_env.filters['zip'] = zip
-
-
-@app.route('/', methods=['GET','POST'])
-def root():
-    jsons = Persons()
-    jsons = jsons.find()
-    result=[]
-    print(jsons)
-    for person in jsons:
-        person['_id'] = str(person['_id'])
-        result.append(person)
-
-    return jsonify(result)
+app.jinja_env.filters['zip'] = zip
 
 @app.route('/test', methods=['POST','GET'])
 def test():
-    return "Api working"
+    string = "Api working"
+    return string
 
 @app.route('/login', methods=['POST','GET'])
 def login():
@@ -53,7 +35,11 @@ def login():
         #else:
             #print("wrong")
 
-    return render_template('templates/login.html')
+    return render_template("login.html")
+
+@app.route('/index', methods=['POST','GET'])
+def index():
+    return render_template("index.html")
 
 @app.route('/register', methods=['POST','GET'])
 def registro():
@@ -76,10 +62,53 @@ def registro():
         id = id + 1
         #a.insert_user(id,email,name,company,age,phone)
 
-        return redirect(url_for('login'))
+        return redirect(url_for('login.html'))
 
-    return render_template('templates/register.html')
+    return render_template('register.html')
+
+@app.route('/obtener')#, methods=['GET'])
+def obtener():
+    mongo_uri = "mongodb+srv://Tayran:admin123@cluster0-quyop.gcp.mongodb.net/test?retryWrites=true"
+    
+    mongo_client = MongoClient(mongo_uri)
+    mongo_db = mongo_client.db
+    mongo_collectionPerson = mongo_db.person
+    
+    #Primera consulta, encontrar peraonas con nombre que contenga con alguna letra"
+    pipe = [{"$match":{"name":{ "$regex": 'K' }}}]
+    
+    cursor = mongo_collectionPerson.aggregate(pipe)
+    
+    result1 = list(cursor)
+    
+    '''pipe2 = [{"$match":{ "name": "Ola Stanley" }}, {"$project":{"_id":0, "knows":1}}];
+        
+        cursor = mongo_collectionPerson.aggregate(pipe2)
+        
+        result2 = list(cursor)'''
+    
+    '''pipe = [{"$match":{"name":"Wendi Mcdonald"}}]
+        pipe2 = [{"$match":{"name":"Martin Hopkins"}}]
+        
+        cursor = mongo_collectionPerson.aggregate(pipe)
+        cursor2 = mongo_collectionPerson.aggregate(pipe2)
+        
+        result3 = list(cursor)
+        result4 = list(cursor2)'''
+    
+    cursor = mongo_collectionPerson.distinct("knows", {"_id":1})
+    result2 = list(cursor)
+    
+    temp = result2[0]
+    temp2 = temp[1]
+    temp3 = temp[3]
+    
+    result3 = list(mongo_collectionPerson.find({"_id":int(temp2)}))
+    
+    result4 = list(mongo_collectionPerson.find({"_id":int(temp3)}))
+    
+    return [result1, result3, result4]
 
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", debug=True)
