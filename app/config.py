794f2780941a06409b8c3daee6967bff392d61de
PTeran <pablo_teran_rios@hotmail.com>
2019-05-08 20:28:19 -0500

    Final

--- app/config.py
+++
@@ -1,8 +0,0 @@
-# --- On cloud resources ---
-# Mongo configuration
-MONGO_URI = 'mongodb+srv://Tayran:admin123@cluster0-quyop.gcp.mongodb.net/test?retryWrites=true'
-
-# Redis configuration
-REDIS_HOST = 'redis-19187.c124.us-central1-1.gce.cloud.redislabs.com'
-REDIS_PORT = 19187
-REDIS_PASSWORD = 'LwePgFWKaCrm0ht4u8mPQliPIWJaodut'
